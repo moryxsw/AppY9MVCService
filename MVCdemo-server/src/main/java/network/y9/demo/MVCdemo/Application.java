8be093cb3a189237a12a4ccfde9a6fbd257c4893
moryxsw <moryxsw@outlook.com>
2023-07-31 22:06:34 +0800

    feat:初始化模板

---
+++ MVCdemo-server/src/main/java/network/y9/demo/MVCdemo/Application.java
@@ -0,0 +1,19 @@
+package network.y9.demo.MVCdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author moryxsw
+ * @since 2023/7/31
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
